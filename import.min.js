6ca072857282612abacc28cdc44ac0ffb99d8bf9
Greg McLeod <cleod9@gmail.com>
2015-03-24 23:12:05 -0400

    -Fixed copyright

--- import.min.js
+++ import.min.js
@@ -3,11 +3,9 @@
   
     A basic package-structuring import system for JavaScript Objects.
   
-    Copyright (c) 2013 Greg McLeod  (Email: cleod9{at}gmail.com)
-
   The MIT License (MIT)
 
-  Copyright (c) 2013 Greg McLeod <cleod9@gmail.com>
+  Copyright (c) 2015 Greg McLeod <cleod9{at}gmail.com>
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
